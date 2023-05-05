0a69e3e263f7ae67ee916e74582a1ece5aae143c
saurabhraj123 <me4saurabh4u@gmail.com>
2023-05-06 00:51:13 +0530

    v1.0

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 const { spawn } = require('child_process');
+const cors = require('cors');
 const express = require('express')
 const app = express();
+
+app.use(cors());
 
 const pythonScript = './sentiment.py';
 
 app.get('/', (req, res) => {
-    // res.send('Hello World');
-    console.log('I am here');
     const text = req.query.text;
 
-    if (!text) return res.send('Invalid input.');
+    if (!text) return res.json({ "error": 'Invalid input.' });
 
     const child = spawn('python', [pythonScript, text]);
     child.stdout.on('data', (data) => {
         const result = data.toString();
-        console.log(result);
-        // console.log('I am here2');
-        res.send(result);
+        const output = { result }
+
+        return res.json(JSON.stringify(output));
     });
 
 
